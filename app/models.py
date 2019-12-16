dfa90c5ca686042dad6657843c50494062787213
Parth Shandilya <parth1989shandilya@gmail.com>
2019-12-16 20:35:38 +0530

    Add models for existing tables

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy.schema import PrimaryKeyConstraint
+from app import db
+
+class Web(db.Model):
+    __tablename__ = "webs"
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text, unique=True, nullable=False)
+    last_updated = db.Column(db.DateTime)
+    pages = db.relationship('Page', backref='web', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Web {}>'.format(self.url)
+
+
+class Page(db.Model):
+    __tablename__ = "pages"
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text, unique=True, nullable=False)
+    html = db.Column(db.Text)
+    error = db.Column(db.Integer)
+    old_rank = db.Column(db.Float)
+    web_id = db.Column(db.Integer, db.ForeignKey("webs.id"))
+
+    def __repr__(self):
+        return f"<Page url={self.url}, new_rank={self.new_rank}, web={self.web}>"
+
+
+class Link(db.Model):
+    __tablename__ = "links"
+    from_id = db.Column(db.Integer, db.ForeignKey("pages.id"))
+    to_id = db.Column(db.Integer, db.ForeignKey("pages.id"))
+
+    __table_args__ = (PrimaryKeyConstraint(from_id, to_id),)
+
+    from_url = db.relationship("Page", foreign_keys=[from_id])
+    to_url = db.relationship("Page", foreign_keys=[to_id])
+
+    def __repr__(self):
+        return f"<Link from {self.from_url} -to-> {self.to_url}>"
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
