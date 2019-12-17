826295d45078f0a6ab5a6d68829c545d140bbeea
Parth Shandilya <parth1989shandilya@gmail.com>
2019-12-18 05:15:22 +0530

    Added Forms and alert notifications

--- app/routes.py
+++ app/routes.py
@@ -14,21 +14,33 @@ from app.spider.spider_node import spider_node_func
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        web_url = request.form["webUrl"]
-        num_pages = request.form["numPages"]
-        num_iter = request.form["iterno"]
-        web_url_node = request.form["webUrlnode"]
-        perpexi = request.form["perp"]
-        if web_url:
+        try:
+            web_url = request.form["webUrl"]
+            num_pages = request.form["numPages"]
+            num_iter = request.form["iterno"]
+            # Add URL Validation
+            flash('Process started!', 'success')
             spider_func(web_url, num_pages)
+            flash('Scrapped the Pages!', 'success')
+            flash('Page Rank Calculation Started!', 'success')
             sprank_func(domain=web_url, num_iterations=num_iter)
+            flash('Page Rank Calculation Ended!', 'success')
+            flash('Visualizing Graph!', 'success')
             spjson_func(domain=web_url)
-            return render_template('index.html', title='Home', result=False)
-        elif web_url_node:
+            return render_template('index.html', title='Home', result=True)
+        except KeyError:
+            web_url = None
+        try:
+            web_url_node = request.form["webUrlnode"]
+            perpexi = request.form["perp"]
+            # Add URL Validation
+            flash('Visualizing Node Embedings!', 'success')
             spider_node_func(domain=web_url_node, perpex=perpexi)
             return render_template('index.html', title='Home', result=True)
-        else:
-            error = 'Please try again! Not a valid Web URL'
-            return render_template('index.html', error=error)
+        except KeyError:
+            web_url_node = None
+
+        error = 'Please try again! Not a valid Web URL'
+        return render_template('index.html', error=error, result=False)
     else:
         return render_template('index.html', result=False)
