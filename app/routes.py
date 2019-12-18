789a8c413b4611dab11d6e2f710a69a12e180582
Parth Shandilya <parth1989shandilya@gmail.com>
2019-12-18 05:33:00 +0530

    Added input fields  for Node2Vec

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,7 @@ def index():
             web_url = request.form["webUrl"]
             num_pages = request.form["numPages"]
             num_iter = request.form["iterno"]
+            node_iter = request.form["itno"]
             # Add URL Validation
             flash('Process started!', 'success')
             spider_func(web_url, num_pages)
@@ -26,16 +27,26 @@ def index():
             sprank_func(domain=web_url, num_iterations=num_iter)
             flash('Page Rank Calculation Ended!', 'success')
             flash('Visualizing Graph!', 'success')
-            spjson_func(domain=web_url)
+            spjson_func(domain=web_url, howmany=node_iter)
             return render_template('index.html', title='Home', result=True)
         except KeyError:
             web_url = None
+
         try:
             web_url_node = request.form["webUrlnode"]
             perpexi = request.form["perp"]
+            embed_dimm = request.form["emb"]
+            walk_length = request.form["node"]
+            num_walks = request.form["walk"]
             # Add URL Validation
             flash('Visualizing Node Embedings!', 'success')
-            spider_node_func(domain=web_url_node, perpex=perpexi)
+            spider_node_func(
+                domain=web_url_node,
+                perpex=perpexi,
+                embed_dimm=embed_dimm,
+                walk_length=walk_length,
+                num_walks=num_walks
+            )
             return render_template('index.html', title='Home', result=True)
         except KeyError:
             web_url_node = None
